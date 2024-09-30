aae2fa2c733a769f8df6a50aac293c1449ecbc5d
RivanDioPerdinan <rivandio81@gmail.com>
2024-09-30 09:48:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/iyowapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.iyowapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Iyowapp());
+}
+
+class Iyowapp extends StatelessWidget {
+  const Iyowapp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Placeholder();
+  }
+}
